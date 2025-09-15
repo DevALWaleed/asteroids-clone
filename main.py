ec585759defe0545b50f425271794086e482aab8
DevALWaleed <mrwaleedhweml@gmail.com>
2025-09-15 18:18:44 +0300

    Implement rotationto the spaceship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -8,18 +9,20 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while (1):
         # This will check if the user has closed the window and exit the game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.Surface.fill(screen, "black")
+        player.draw(screen)
         pygame.display.flip()
+        player.update(dt)
 
         dt = clock.tick(60) / 1000 # limit fps to 60
-        
-
 
 if __name__ == "__main__":
     main()
