bd8a6c31e4bf3ec004767fe1f0dbf83bdbfceef6
DevALWaleed <mrwaleedhweml@gmail.com>
2025-09-15 15:05:38 +0300

    Initialize an empty window

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from asteroids-clone!")
+import pygame
+from constants import *
 
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Displaying a window
+
+    while (1):
+        # This will check if the user has closed the window and exit the game loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
