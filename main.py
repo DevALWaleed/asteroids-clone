ef230c9f2d62cf5280f2fb9d488261eec89dd2c4
DevALWaleed <mrwaleedhweml@gmail.com>
2025-09-16 10:52:21 +0300

    Finishing the game

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import sys
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+from shooting import *
 
 def main():
     pygame.init()
@@ -9,7 +13,20 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    Player.containers = (updatable, drawable) 
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    Shot.containers = (shots, updatable, drawable)
 
     while (1):
         # This will check if the user has closed the window and exit the game loop
@@ -18,9 +35,21 @@ def main():
                 return
             
         pygame.Surface.fill(screen, "black")
-        player.draw(screen)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
-        player.update(dt)
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision_detect(player):
+                print("Game over!")
+                sys.exit()
+
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collision_detect(shot):
+                    asteroid.split()
+                    shot.kill()
 
         dt = clock.tick(60) / 1000 # limit fps to 60
 
